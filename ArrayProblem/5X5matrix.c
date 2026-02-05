3f6c60a4f8de1feaf97215f0be7abac90fb509da
Minhajul43 <minhajul255685islam@gmail.com>
2026-02-05 08:04:38 +0600

    change

--- ArrayProblem/5X5matrix.c
+++ ArrayProblem/5X5matrix.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int main()
-{
-  int max[5][5];
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
+int main(){
+  int max[3][3];
+  printf("Enter the array data:");
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
       scanf("%d",&max[i][j]);
     }
   }
 
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
       printf("  %d",max[i][j]);
     }
     printf("\n");
